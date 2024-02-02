093a2379b98e4a0558497e38b6c328bccc0b9637
First user <140979441+dmrey01@users.noreply.github.com>
2024-02-01 22:53:59 -0600

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # sqlalchemy_challenge
 Module 10 challege 
+    Created a new repository called sqlalchemy-challenge
+    created a new folder SurfsUp
+    Added folder from Module 10
+    Started with Jupyter Notebook 
+     imported, numpy, padas, datetime,
+     imported, sqlalchemy, automap,
+     from sqlalchemy import create_engine,func.
+
+     created the connection by engine = create_engine to the resource folder
+     added the reflection to exisitng data base and added the reflecting tables
+        Base=auto_base()
+            Base.prepare(autoload_with=engine)
+    Found the class keys
+    Saved them in a reference table 
+    created a session link form our Python to our database. 
+
+    Explored Analysis of Precipitation
+    Found the most recent date of 2017-08-23
+
+    Designed a query to retrieve the last 12 months of precipiations data and plot the results. 
+    calulated the date one year form th last date
+    preform a query and retrieve the data and preciepitation scores. See output line 20 for graph. 
+
+    Calculate the summary statistics for the precipitation data.  See output in line 21. 
+
+    Exploratiory Station Analysis. 
+
+    Found total number of stations in the dataset = 9
+
+     Query and found the most active stations in the dataset. 
+        this we used the func.count and group_by along with the order_by methods. Then put the results in descending order. 
+
+    Using the station Id's queried the lowest, hiest and avg temperature.  see results in output line #24
+
+    Query using most tools to of the most active station we ploted the temperature for the last 12 months. se line 81 for the output. and ploted it using a Histogram. 
+
+    I did encounter one issue with this dataset.  The sqlite file became correpted.  after I completed this first part.  I had to re-install the file and re-run the complete data set.  
+
+Part 2
+
+started working on the FLASK 
+had issues with the dataset had to redo my data twice.  the file was corrupt. Then I recieved a lint error which Cait our insturtor helped me solved. 
+
+Hoepefully everything is working now as I had to reload the file again at the end. 
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,150 @@
 # Import the dependencies.
-
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
+#print(Base.classes.keys())
 
 # Save references to each table
-
+Measurement=Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+app = Flask(__name__)
+
+##################################################
+# Flask Routes 
+##################################################
+
+# 1 Home pg. 
+#list all avaiable routes
+@app.route("/")
+def welcome():
+    """List all avaiable routes!"""
+    return(
+        f"Avaiable Routes:<br/>"
+        f"/api.v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"api/v1.0<start>/br/>"
+        f"api/v1.0/<start>/<end>br/>"
+)
+
+#2 Return json with the date as the key and the value as the precipitation
+# precipitation route  returns the jsonified preipitation data for the last year in the database. 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session=Session(engine)
+    one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+    results = session.query(Measursement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all() 
+
+    prcp_data =[]
+    for date, prcp in results:
+        prcp_dict ={
+            "date": date,
+            "prcp": prcp
+        }
+        prcp_data.append(prcp_dict)
 
 
 
+    session.close()
+    return jsonify(prcp_data)
 
-#################################################
-# Flask Routes
-#################################################
+#3 return a station route - jsonified data of all of the station in the database
+@app.route("/api/v1.0/station")
+
+def station():
+    session = Session(engine)
+    """Return information on all the stations"""
+    results = session.query(Station.station, Station.name, Station.id, Station.latatuide, Station.longitude, Station.elevation).all()
+
+
+    station_info =[]    
+    for station, name, id, latitude, longitude, elevation  in results:
+        station_dict = {
+        "station" : station,
+        "name" : name,
+        "id" : id,
+        "latitude" : latitude,
+        "longitude": longitude,
+        "elevation": elevation
+    }  
+    station_info.append(station_dict)
+
+    session.closed()
+
+    return jsonify(station_info)
+
+#4 tobs route returns jsonfied data for the most active station (USC00519281) and make sure you only
+#   return the jsonifeid data for the last year of data. 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session =Session(engine)
+    
+    one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results =session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= one_year_ago).filter(Measurement.station == 'USC00519281').all()
+    
+    tobs_data =[]
+    for date, tobs in results:
+        tobs_dict ={
+            "date": date,
+            "tobs": tobs
+    }
+    tobs_data.append(tobs_dict)
+    session.close()
+    
+    return jsonify(tobs_data)
+
+#Returns jsonified data as a paramater for the url
+#returns the min, max and avg temperture calculation from the start date and end of the dataset. 
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+
+def temperature_starts(start, end=None):
+    session =Session(engine)
+    start_date =dt.datetime.strptime(start, "%Y-%m-%d")
+    
+    if end:
+        end_date =session.query(func.max(Measurement.date)).scalar()
+    
+#query min, max and avg
+    temp_stats =session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)\
+        .filter(Measurement.date >= start_date).filter(Measurement.date <=end_date).all())
+
+    session.close()
+
+    temp_dict ={
+        "start_date": start_date.strftime("%Y-%m-%d"),
+        "end_date": end_date.strftime("%Y-%m-%d") if end_date else None,
+        "min_temperature": temperature_stats[0][0],
+        "max_temperature": temperature_stats[0][1],
+        "avg_temperature": temperature_stats[0][2]
+        }
+
+    session.close()
+
+    return jsonify(temp_dict)                                
+
+if __name__=="__main__":
+    app.run(debug=True)
